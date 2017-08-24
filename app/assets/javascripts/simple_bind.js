5390a0374ee99f59ba41daacd2a54ec84beae01a
Jeff Emminger <jeff@7compass.com>
2017-08-24 17:17:06 -0400

    convert jquery to vanilla js

--- app/assets/javascripts/simple_bind.js
+++ app/assets/javascripts/simple_bind.js
@@ -1,32 +1,77 @@
 this.simpleBind = function() {
-  $('input:text').keyup(function() {
-    var id = $(this).data('bind');
-    if (id){ $("[bind='" + id + "']").html($(this).val()) }
-  });
 
-  $('select').change(function() {
-    var id = $(this).data('bind');
-    if (id){ $("[bind='" + id + "']").html($(this).find("option:selected").text()) }
-  });
-  
-  $('input[type=checkbox]').change(function() {
-    var id = $(this).data('bind');
-    if (id){ 
-      if($(this).is(":checked")) {
-        $("[bind='" + id + "']").prop('checked', true);
-      } else {
-        $("[bind='" + id + "']").prop('checked', false);
+    // select a list of matching elements, context is optional
+    // return an array
+    function $(selector, context) {
+      var context = context || document;
+      var selectorType = 'querySelectorAll';
+      var is_single_element = selector.indexOf('#') === 0;
+
+      if (is_single_element) {
+        selectorType = 'getElementById';
+        selector = selector.substr(1, selector.length);
+      }
+
+      var results = context[selectorType](selector);
+      if (is_single_element) {
+        return results;
+      }
+      else {
+        return Array.from(results);
       }
     }
-  });
-};
 
-if (typeof defined === "function" ? defined(Turbolinks) : void 0) {
-  $(document).on('turbolinks:load', function() {
-    simpleBind();
-  });
-} else {
-  $(document).on('ready', function() {
-    simpleBind();
-  });
-}
+
+    $('input[type=text]').forEach(function(input){
+      input.addEventListener('keyup', function(evt) {
+        var input = evt.target;
+        var id = input.getAttribute('data-bind');
+        if (id){
+          $("[bind='" + id + "']").forEach(function(el){
+            el.innerHTML = input.value;
+          });
+        }
+      });
+    });
+
+
+    $('select').forEach(function(sel){
+      sel.addEventListener('change', function(evt) {
+        var sel = evt.target;
+        var id = sel.getAttribute('data-bind');
+        if (id){
+          $("[bind='" + id + "']").forEach(function(el){
+            var value = [];
+            $('option:checked', sel).forEach(function(opt){
+              value.push(opt.value);
+            });
+
+            el.innerHTML = value.join(', ');
+          });
+        }
+      });
+    });
+
+
+    $('input[type=checkbox]').forEach(function(input){
+      input.addEventListener('change', function(evt) {
+        var id = input.getAttribute('data-bind');
+        if (id){
+          $("[bind='" + id + "']").forEach(function(cb){
+            cb.checked = input.checked;
+          });
+        }
+      });
+    });
+
+  };
+
+  if (typeof defined === "function" ? defined(Turbolinks) : void 0) {
+    document.addEventListener('turbolinks:load', function() {
+      simpleBind();
+    });
+  } else {
+    document.addEventListener('DOMContentLoaded', function() {
+      simpleBind();
+    });
+  }
